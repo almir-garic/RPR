9f1c8fbd8352f4cacfa79acc0172af3dccc56cb6
almir-garic <almir.garic.1992@ieee.org>
2015-10-23 15:19:22 -0700

    Tut1zad2

---
+++ Tut1zad2/Tut1zad2/Osoba.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace Tut1zad2
+{
+    /// <summary>
+    /// Definicija Klase Osoba
+    /// </summary>
+    public class Osoba
+    {
+        private string ime;
+        private string prezime;
+
+        /// <summary>
+        /// Defaultni konstruktor,parent(tj. base) klasa treba da ga sadrzi 
+        /// </summary>
+        public Osoba()
+        {
+            ime = "default ime";
+            prezime = "default prezime";
+        }
+        /// <summary>
+        /// Instance Konstruktor klase Osoba
+        /// </summary>
+        /// <param name="ime">privatni parametar ime</param>
+        /// <param name="prezime">privatni parametar prezime</param>
+        public Osoba(string parametar_ime, string parametar_prezime, int parametar_brojStudenata)
+        {
+            Ime = parametar_ime;
+            Prezime = parametar_prezime;
+        }
+
+        /// <summary>
+        /// property-get i set za ime
+        /// </summary>
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+            set
+            {
+                ime = value;
+            }
+        }
+
+        /// <summary>
+        /// property-get i set za prezime
+        /// </summary>
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+            set
+            {
+                prezime = value;
+            }
+        }
+
+    }
+}
---
+++ Tut1zad2/Tut1zad2/Predmet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace Tut1zad2
+{
+
+
+
+    /// <summary>
+    /// Javna klasa Predmet
+    /// </summary>
+    public class Predmet : Osoba
+    {
+        string nazivPredmeta;
+        Osoba profesor;
+        int brojStudenata;
+
+        public Predmet(string parametar_nazivPredmeta, string parametar_ime, int parametar_brojStudenata)
+        {
+            NazivPredmeta = parametar_nazivPredmeta;
+            Ime = parametar_ime;//nasljedjeni privatni atribut ime iz klase osoba,dodjeluje se child klasi Predmet
+            BrojStudenata = parametar_brojStudenata;
+        }
+
+        /// <summary>
+        /// property-get i set za nazivPredmeta
+        /// </summary>
+        public string NazivPredmeta
+        {
+            get
+            {
+                return nazivPredmeta;
+            }
+            set
+            {
+                nazivPredmeta = value;
+            }
+        }
+
+        /// <summary>
+        /// property-get i set za brojStudenata
+        /// </summary>
+        public int BrojStudenata
+        {
+            get
+            {
+                return brojStudenata;
+            }
+            set
+            {
+                brojStudenata = value;
+            }
+        }
+
+    }
+}
