7230ff408f310a24cf1949cfe664aca50b32f950
almir-garic <almir.garic.1992@ieee.org>
2015-11-01 16:32:34 -0700

    Tut1SRzad4

---
+++ Tut1SRzad4/Tut1SRzad4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Autor : Almir Garic (almir.garic.1992@ieee.org)
+/*Zadatak 4
+Napišite program koji traži da se sa tastature unese prirodan broj n, a nakon toga se unose elementi
+kvadratne matrice formata n×n. 
+Program nakon toga treba da ispiše redni broj kolone sa najvećom sumom elemenata, redni broj reda sa
+najmanjom sumom elemenata, kao i sumu elemenata na dijagonali. Koristiti jagged ili
+multidimenzionalni niz.
+Reference:
+Jagged nizovi: http://msdn.microsoft.com/en-us/library/vstudio/2s05feca.aspx
+Multidimenzionalni nizovi: http://msdn.microsoft.com/en-us/library/vstudio/2yd9wwz4.aspx
+Metode klase string: http://msdn.microsoft.com/en-us/library/system.string_methods(v=vs.110).aspx*/
+
+namespace Tut1SRzad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
+
+}
