56769ffa5d06741bd8da2b0d5b9cb5658d9a7325
almir-garic <almir.garic.1992@ieee.org>
2015-11-01 20:38:23 -0700

    Tut1SRzad7

---
+++ Tut1SRzad7/Tut1SRzad7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Autor : Almir Garic(almir.garic.1992@ieee.org)
+/*Zadatak 7
+Napišite program koji traži od korisnika da unese spisak riječi (broj riječi se prethodno unosi sa
+tastature), a zatim ispisuje na ekran prvu i posljednju riječ iz spiska po abecednom poretku, kao i popis
+svih unesenih riječi, ali bez ispisivanja duplikata (tj. bez ispisivanja riječi koje su se već jednom ispisale).
+Program realizirajte korištenjem niza stringova, odnosno niza čiji su elementi tipa “string”.*/
+
+namespace Tut1SRzad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
