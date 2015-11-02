9242d08a5e315a5e48c0297904e0cacf8bc5b967
almir-garic <almir.garic.1992@ieee.org>
2015-11-01 21:23:45 -0700

    Tut1SRzad7

--- Tut1SRzad7/Tut1SRzad7/Program.cs
+++ Tut1SRzad7/Tut1SRzad7/Program.cs
@@ -17,6 +17,54 @@ namespace Tut1SRzad7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Unesi broj rijeci ");
+            string s = Console.ReadLine();
+            var n = Int32.Parse(s);
+
+            //inicijaliziramo niz stringova
+            string[] rijecnik = new string[n];
+
+            //instanciramo clanove niza stringova
+            for(int i = 0; i < rijecnik.Length; i++)
+            {
+                rijecnik[i] = Console.ReadLine();
+            }
+
+            //istanciramo sortirani niz
+            string[] sortiranRijecnik = new string[n];
+            for (int i = 0; i < rijecnik.Length; i++)
+            {
+                sortiranRijecnik[i] =rijecnik[i];
+            }
+
+            //funkcija za sortiranje niza abecedno stringa preuzeta sa
+            //http://stackoverflow.com/questions/16636554/sorting-an-array-alphabetically-in-c-sharp
+            Array.Sort(sortiranRijecnik, (x, y) => String.Compare(x, y));
+
+            //ispisujemo clanove niza clanove niza stringova
+            Console.WriteLine("Popis svih unesenih rijeci je sljedeci :");
+            for (int i = 0; i < rijecnik.Length; i++)
+            {
+                Console.WriteLine(rijecnik[i]);
+            }
+
+            //uz pretpostavku da idu jedna za drugom(mislim na rijeci duplikate)
+            //ispisujemo clanove sortiranog niza clanove niza stringova
+            Console.WriteLine("Popis svih unesenih rijeci po abecednom poretku je sljedeci :");
+            for (int i = 1; i <= sortiranRijecnik.Length; i++)
+            {
+                for(int j=i+1;j<=sortiranRijecnik.Length-1;j++)
+                    if (sortiranRijecnik[i] == sortiranRijecnik[j]) { continue; }
+                    else
+                    {
+                        Console.WriteLine(sortiranRijecnik[i]);
+                        i=j;
+                    }                
+            }
+
+
+            //zaustavljamo konzolu
+            Console.ReadKey();
         }
     }
 }
