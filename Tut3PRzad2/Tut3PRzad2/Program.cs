35d3a0d6893877efb27ed9fdf75edc381d94c815
almir-garic <almir.garic.1992@ieee.org>
2015-11-14 16:59:10 -0700

    Tut3PRzad2

---
+++ Tut3PRzad2/Tut3PRzad2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut3PRzad2
+{
+   
+    class Program
+    {
+
+        static void DodavanjeElemenanta(List<string> rijeci)
+        {
+            rijeci.Add("test");
+            rijeci.Add("asdf");
+            //string dvijeRijeci=new string[]{"predznanja","zadnja"};
+            rijeci.AddRange(new string[] { "predznanja", "zadnja" });//na kraj liste se dodaju dvije rijeci
+                                                                     //iteracije kroz listu pomocu for each petlje 
+            foreach (string s in rijeci)
+            {
+                Console.WriteLine(s + (rijeci.Last().Equals(s) ? Environment.NewLine : ", "));
+                rijeci.Clear();//brisanje svih rijeci iz liste rijeci
+                Console.WriteLine("Ukupno elemenata u listi : " + rijeci.Count);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> rijeci = new List<string>();//instancira se nova lista stringova
+            DodavanjeElemenanta(rijeci);            
+        }
+    }
+}
