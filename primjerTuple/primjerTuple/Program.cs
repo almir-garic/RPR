fa3f7bfc9a95c76388acee77d05791a6bbc254be
almir-garic <almir.garic.1992@ieee.org>
2015-11-28 02:06:18 -0700

    primjerTuple

--- primjerTuple/primjerTuple/Program.cs
+++ primjerTuple/primjerTuple/Program.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace primjerDictionary
+namespace primjerTuple
 {
     class Program
     {
-        //------------------------------KLASA OSOBA I GLUMAC-----------------------------
+        //-------------------------KLASA OSOBA I GLUMAC-------------------------------
         class Osoba
         {
             private string _Ime, _Prezime;
@@ -53,60 +53,62 @@ namespace primjerDictionary
                     Console.Write(s + (_TopFilmovi.Last().Equals(s) ? Environment.NewLine : ", "));
             }
         }
-        //-------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------
+        /// <summary>
+        /// MAIN 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //deklarisem rijecnik
-            Dictionary<string, Glumac> glumci = new Dictionary<string, Glumac>();
+            //Deklaracija rijecnika sa Tuple<string,int> --kljucevima
+            Dictionary<Tuple<string, int>, Glumac> glumci = new Dictionary<Tuple<string, int>, Glumac>();
 
-            //dodajem u rijecnik sa kljucevima
-            glumci.Add("prvi",new Glumac()
+            Tuple<string, int> kljuc1 = Tuple.Create<string, int>("Jimmy", 1);
+            Tuple<string, int> kljuc2 = Tuple.Create<string, int>("Jimmy", 2);
+
+            //dodajem glumce
+            glumci.Add(kljuc1, new Glumac()
             {
                 Ime = "almir",
-                Prezime = "Garic",
+                Prezime = "garic",
                 Starost = 23,
-                TopFilmovi = new List<string>() { "Interstellar", "Magic Mike", "Dallas Buyers Club" }
+                TopFilmovi = new List<string>() { "film1", "film2", "film3" }
             });
-            glumci.Add("drugi",new Glumac()
-            {
-                Ime = "kenan",
-                Prezime = "husic",
-                Starost = 22,
-                TopFilmovi = new List<string>() { "avion", "auto", "tramvaj" }
+        glumci.Add(kljuc2, new Glumac()
+        {
+            Ime = "Matthew",
+            Prezime = "McConaughey",
+            Starost = 45,
+            TopFilmovi = new List<string>() { "Interstellar", "Dallas Buyers Club", "Magic Mike" }
             });
 
             //pomocna varijabla
             Glumac g;
-
-            //mijenjam vrijednost imena
-            if (glumci.TryGetValue("prvi",out g))
+            if(glumci.TryGetValue(Tuple.Create<string,int>("Jimmy",1),out g))
             {
                 g.Ime = "nabukodonosor";
             }
 
-            //ispis kljuceva
-            foreach(string key in glumci.Keys)
+            //ispisi kljuceve
+            foreach(Tuple<string,int>key in glumci.Keys)
             {
-                Console.WriteLine(key);
+                Console.WriteLine(key.Item1 + " " + key.Item2);
             }
 
-            //ispis vrijednosti .Ispisi()
+            //ispisi vrijednosti
             foreach(Glumac value in glumci.Values)
             {
                 value.Ispisi();
             }
-            Console.Read();
 
+            //drugi naci
+            foreach(KeyValuePair<Tuple<string,int>,Glumac> pair in glumci)
+            {
+                pair.Value.Ispisi();
+            }
 
+            //zaustavi konzolu
+            Console.ReadKey();
         }
-    }
 }
-
-/*Ova metoda je optimizirana da vrši provjeru i dodijeljivanje vrijednosti u jednom prolazu što omogućava
-maksimalno efikasan rad sa riječnikom. Iteracija kroz sve ključeve ili vrijednosti iz riječnika moguća je
-pomoću foreach petlje nad atributima Keys i Values, no za iteraciju kroz kompletnu strukturu
-riječnika preporučljivo je koristiti generičku klasu KeyValuePair (foreach (KeyValuePair<T1,
-T2> pair in dictionary)).
-U situacijama kada se ključ riječnika sastoji iz više podataka (npr. String i Integer) moguće je
-definirati riječnik u riječniku, gdje će ključ prvog riječnika biti string, a njegova vrijednost novi riječnik čiji
-je ključ Integer. Ipak, u ovakvim situacijama jednostavnije je koristiti klasu Tuple.*/
+}
