3f10605c98f43d36622ab317f535f716792f0740
almir-garic <almir.garic.1992@ieee.org>
2015-11-28 01:00:14 -0700

    primjerArrayList

---
+++ primjerArrayList/primjerArrayList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace primjerArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList Lista = new System.Collections.ArrayList();
+            Lista.Add("beast");
+            Lista.Add(666);
+            Lista.Add("Eddie");
+            Lista.Add(0.11);
+            Lista.Add('c');
+
+            foreach(object o in Lista) { Console.WriteLine(o); }
+            Console.ReadLine();
+        }
+    }
+}
